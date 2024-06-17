e7820e080d6831803a12e6ef0d1f001d94181460
Eduardo R. Duraes <eduardoduraes.bsi@gmail.com>
2024-06-17 15:31:56 -0300

    fim do capitulo de formularios

--- resources/views/produto/novo.blade.php
+++ resources/views/produto/novo.blade.php
@@ -16,19 +16,19 @@
         @endif
         <div class="mb-3 form-group">
             <label for="nome" class="form-label">Nome: </label>
-            <input id="nome" name="nome" type="text" class="form-control" placeholder="Nome do produto"/>
+            <input id="nome" name="nome" type="text" class="form-control" value="{{old('nome')}}" placeholder="Nome do produto"/>
         </div>
         <div class="mb-3 form-group">
             <label for="descricao" class="form-label">Descrição: </label>
-            <input id="descricao" name="descricao" type="text" class="form-control" placeholder="Descrição do produto"/>
+            <input id="descricao" name="descricao" type="text" class="form-control" value="{{old('descricao')}}" placeholder="Descrição do produto"/>
         </div>
         <div class="mb-3 form-group">
             <label for="preco" class="form-label">Preço: </label>
-            <input id="preco" name="preco"  type="number" class="form-control" placeholder="Preço do produto"/>
+            <input id="preco" name="preco"  type="number" class="form-control" value="{{old('preco')}}" placeholder="Preço do produto"/>
         </div>
         <div class="mb-3 form-group">
             <label for="saldo" class="form-label">Saldo: </label>
-            <input id="saldo" name="saldo" type="number" class="form-control" placeholder="Saldo do produto"/>
+            <input id="saldo" name="saldo" type="number" class="form-control" value="{{old('saldo')}}" placeholder="Saldo do produto"/>
         </div>
         <button type="submit" class="btn btn-primary btn-block">SALVAR</button>
     </form>
